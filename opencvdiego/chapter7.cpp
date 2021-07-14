4a34f4d18e83d928b86a0cfa288bc44e9c4f3ab8
Diego <A00827847@itesm.mx>
2021-07-13 20:47:08 -0500

    Chapter 7

--- opencvdiego/chapter7.cpp
+++ opencvdiego/chapter7.cpp
@@ -6,3 +6,37 @@
 //
 
 #include <stdio.h>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+// Chapter 7: Shapes/contour detection
+int main()  {
+    string path = "Resources/shapes.png";
+    Mat img = imread(path);
+    
+    // BGR is convention in OpenCV,  like RGB
+    cvtColor(img, img, COLOR_BGR2GRAY); // Change from colored to grayscale
+    
+    // Noise reduction
+    GaussianBlur(img, img, Size(3,3), 3,0);
+    
+    // Edge detection algorithm; we have to remove noise first
+    Canny(img, img, 20, 150);
+    
+    // These are primarily used to detect the shape of an object
+    // Some edges are not connected, so we can dilate the edges to make them thicker
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(5,5)); // Only use odd numbers for size
+    dilate(img, img, kernel);
+    
+    
+    
+    imshow("Image", img);
+    waitKey(0);
+    return 0;
+}
+
