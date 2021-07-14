de9d28729d8fc026d1f13488c10c634a67cc0a6a
Diego <A00827847@itesm.mx>
2021-07-14 11:08:35 -0500

    created getContours func

--- opencvdiego/chapter7.cpp
+++ opencvdiego/chapter7.cpp
@@ -14,25 +14,28 @@
 using namespace cv;
 using namespace std;
 
+void getContours(Mat &img) {
+    //findContours(img, contours, hierarchy, RETR_EXTERNAL, <#int method#>);
+}
+
 // Chapter 7: Shapes/contour detection
 int main()  {
     string path = "Resources/shapes.png";
     Mat img = imread(path);
     
+    // Preprocessing
     // BGR is convention in OpenCV,  like RGB
     cvtColor(img, img, COLOR_BGR2GRAY); // Change from colored to grayscale
-    
     // Noise reduction
     GaussianBlur(img, img, Size(3,3), 3,0);
-    
     // Edge detection algorithm; we have to remove noise first
     Canny(img, img, 20, 150);
-    
     // These are primarily used to detect the shape of an object
     // Some edges are not connected, so we can dilate the edges to make them thicker
     Mat kernel = getStructuringElement(MORPH_RECT, Size(5,5)); // Only use odd numbers for size
     dilate(img, img, kernel);
     
+    getContours(img);
     
     
     imshow("Image", img);
